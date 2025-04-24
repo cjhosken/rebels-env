250ce76d6b3b457e13353e334b114dbddbd6b6c5
cjhosken <hoskenchristopher@gmail.com>
2025-04-24 14:32:12 +0100

    splash update

--- scripts/rblSplash.sh
+++ scripts/rblSplash.sh
@@ -5,9 +5,14 @@ echo
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Default values for the arguments
-DEFAULT_TITLE="RENDERING"
-DEFAULT_SUBTITLE="Please do not touch!"
-DEFAULT_CONTACT="The REBELS Team"
+DEFAULT_TITLE="REBELS (FMP)"
+DEFAULT_SUBTITLE="Rendering! Please do not touch!"
+DEFAULT_CONTACT="""
+s5605094@bournemouth.ac.uk
+s5603506@bournemouth.ac.uk
+s5644639@bournemouth.ac.uk
+s5616347@bournemouth.ac.uk
+"""
 
 # If arguments are provided, use them, otherwise use the default values
 TITLE="${1:-$DEFAULT_TITLE}"

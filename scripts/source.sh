347dff112d9b09d48b3811c5e9882a1ba3288053
cjhosken <hoskenchristopher@gmail.com>
2025-04-30 09:21:31 +0100

    disabled drive

--- scripts/source.sh
+++ scripts/source.sh
@@ -6,22 +6,24 @@ export DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 alias rclone="$DIR/../tools/rclone/rclone"
 
-REBELS_DRIVE="$HOME/REBELS"
-mkdir -p $REBELS_DRIVE
-chmod 700 "$REBELS_DRIVE"
+# DRIVE MOUNTING (BROKEN)
+
+#REBELS_DRIVE="$HOME/REBELS"
+#mkdir -p $REBELS_DRIVE
+#chmod 700 "$REBELS_DRIVE"
 
 # Check if the drive is already mounted
-if ! mountpoint -q "$REBELS_DRIVE"; then
-    if ! rclone mount REBELS: "$REBELS_DRIVE" \
-        --vfs-cache-mode writes \
-        --allow-non-empty; then
-        echo "ERROR: Failed to mount REBELS drive" >&2
-    else
-    echo "REBELS: Root drive is mounted at $REBELS_DRIVE"
-    fi
-fi
-
-export RBL="$REBELS_DRIVE/RBL"
+#if ! mountpoint -q "$REBELS_DRIVE"; then
+#    if ! rclone mount REBELS: "$REBELS_DRIVE" \
+#        --vfs-cache-mode writes \
+#        --allow-non-empty; then
+#        echo "ERROR: Failed to mount REBELS drive" >&2
+#    else
+#    echo "REBELS: Root drive is mounted at $REBELS_DRIVE"
+#    fi
+#fi
+#
+#export RBL="$REBELS_DRIVE/RBL"
 
 alias rblHoudini="$DIR/rblHoudini.sh"
 alias rblComfy="$DIR/rblComfy.sh"

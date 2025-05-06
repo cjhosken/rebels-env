f34f2552b3e0c328f1b5fad63e750b151abb5f8f
cjhosken <hoskenchristopher@gmail.com>
2025-05-06 16:33:52 +0100

    updated rbl scripts

--- scripts/rblNuke.sh
+++ scripts/rblNuke.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export RBL="~/REBELS/RBL"
-export NUKE_PATH="$RBL/05_pipeline/tools/nuke/gizmos":"$RBL/05_pipeline/tools/nuke/scripts":$NUKE_PATH
+export NUKE_PATH="$RBL/05_pipeline/tools/nuke/gizmos":$NUKE_PATH
 
 ~/.ncca/scripts/goNuke.sh "$@"
